8580b0272bbf2d4fcae72ede1eae5f2bb2a01f66
Leonavarro2287 <leonardo.gabriel.navarro@mi.unc.edu.ar>
2026-05-21 14:38:05 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+# modelos_decision/__init__.py
+
 from .linea1_estadistica import run_estadistica
 from .linea2_normalizacion import run_normalizacion
 from .linea3_ponderaciones import run_ponderaciones
@@ -5,13 +7,3 @@ from .linea4_agregacion import run_agregacion
 from .linea5_ahp import run_ahp
 from .linea6_topsis import run_topsis
 from .linea7_rim import run_rim
-
-__all__ = [
-    "run_estadistica",
-    "run_normalizacion",
-    "run_ponderaciones",
-    "run_agregacion",
-    "run_ahp",
-    "run_topsis",
-    "run_rim",
-]
