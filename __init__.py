94f47126af007892249cd2d55216b2a26601cdf4
Leonavarro2287 <leonardo.gabriel.navarro@mi.unc.edu.ar>
2026-05-21 22:56:50 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# modelos_decision/__init__.py
-
-from .linea1_estadistica import run_estadistica
-from .linea2_normalizacion import run_normalizacion
-from .linea3_ponderaciones import run_ponderaciones
-from .linea4_agregacion import run_agregacion
-from .linea5_ahp import run_ahp
-from .linea6_topsis import run_topsis
-from .linea7_rim import run_rim
