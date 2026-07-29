11d612fec7b7cecc71e62e374102d406df66dee5
Leonavarro2287 <leonardo.gabriel.navarro@mi.unc.edu.ar>
2026-07-29 19:39:16 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from .estadistica import run_estadistica
+from .normalizacion import run_normalizacion
+from .ponderaciones import run_ponderaciones
+from .agregacion import run_agregacion
+from .ahp import run_ahp
+from .topsis import run_topsis
+from .rim import run_rim
+
+__all__ = [
+    "run_estadistica",
+    "run_normalizacion",
+    "run_ponderaciones",
+    "run_agregacion",
+    "run_ahp",
+    "run_topsis",
+    "run_rim",
+]
